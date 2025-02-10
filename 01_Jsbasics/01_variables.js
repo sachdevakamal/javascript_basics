c7445d4d36bb9ff7441c1004454dac987d6795d8
sachdevakamal <sachdeva423@gmail.com>
2025-02-10 23:22:38 +0000

    Added some basic details

---
+++ 01_Jsbasics/01_variables.js
@@ -0,0 +1,6 @@
+// Syntax is const/let name of the container = "data" - we also learnt the story of the var thing so we will not use it and if only container is created but no value is assigned then it will be undefined
+// Datatypes and ECMA Standards:
+// Coding sikhne ke liye jaruri h jyada se jyada keyboard pe practice hona and jyada sey jyada code likhna. This will help you become a good coder
+
+//learnt about console.table ([all the IDs can be put here])
+//Code should be readable not just made to have it 
