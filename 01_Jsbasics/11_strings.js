f76f289341c25fb4b966e171e4e80967415ddd07
sachdevakamal <sachdeva423@gmail.com>
2025-02-11 15:22:40 +0000

    new method of console.log

---
+++ 01_Jsbasics/11_strings.js
@@ -0,0 +1,5 @@
+let name = "Kamal"
+let repo = 50
+let emailAddress = "kamal@gmail.com"
+
+console.log(`My name is ${name} and my total repository is ${repo} and my email address is ${emailAddress}`)
